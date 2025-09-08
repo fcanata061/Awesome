ee5f06ad85c1c827f1f2b32185b09f0377c60791
fcanata061 <fcanata61@gmail.com>
2025-09-08 01:14:46 -0300

    Create init.lua

---
+++ circular/ui/widgets/init.lua
@@ -0,0 +1,14 @@
+-- ui/widgets/init.lua
+-- Hub dos widgets circulares animados
+
+local widgets = {}
+
+widgets.cpu     = require("ui.widgets.cpu")
+widgets.mem     = require("ui.widgets.mem")
+widgets.disk    = require("ui.widgets.disk")
+widgets.net     = require("ui.widgets.net")
+widgets.volume  = require("ui.widgets.volume")
+widgets.battery = require("ui.widgets.battery")
+widgets.clock   = require("ui.widgets.clock")
+
+return widgets
