fd242b982bbd154772d4cb578b1d44e44dba8d7e
fcanata061 <fcanata61@gmail.com>
2025-09-08 01:24:14 -0300

    Create autostart.sh

---
+++ circular/autostart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# ~/.config/awesome/all-circles/autostart.sh
+
+# Programas que iniciam junto com o Awesome
+picom --config ~/.config/picom/picom.conf &
+nm-applet &
+volumeicon &
+nitrogen --restore &
