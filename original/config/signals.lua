c87c384da89c50ac4febe17f13b1f10081193208
fcanata061 <fcanata61@gmail.com>
2025-09-08 00:57:04 -0300

    Create signals.lua

---
+++ original/config/signals.lua
@@ -0,0 +1,15 @@
+-- config/signals.lua
+-- Sinais de comportamento das janelas (retrô)
+
+local beautiful = require("beautiful")
+
+-- Foco muda cor da borda
+client.connect_signal("focus",
+    function(c) c.border_color = beautiful.border_focus end)
+client.connect_signal("unfocus",
+    function(c) c.border_color = beautiful.border_normal end)
+
+-- Foco segue o mouse
+client.connect_signal("mouse::enter", function(c)
+    c:emit_signal("request::activate", "mouse_enter", {raise=false})
+end)
